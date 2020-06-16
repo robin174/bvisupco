ed8e9359a56994880265456e9158de542cf4076a
Robin Green <robin@designingreen.co.uk>
2020-06-16 14:03:54 +0100

    Tweaks to faq page

--- page-faq.php
+++ page-faq.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 			    	<div class="grid-container template--page">
 			    		<div class="grid-x grid-padding-x">
-			    			<div class="small-12 medium-4 large-3 cell">
+			    			<div class="small-12 medium-4 large-4 cell">
 			    				<h2>Local</h2>
 			    				<p>For surf and beach conditions in Cane Garden Bay:<br>
 			    				<a href="https://www.resortcams.com/webcams/cane-garden-bay" title="Resort Cams - Cane Garden Bay" target="_blank">www.resortcams.com</a></p>
@@ -22,8 +22,8 @@ get_header(); ?>
 			    				<a href="https://magicseaweed.com/British-Virgin-Islands-Surfing/133/" target="_blank">www.magicseaweed.com</a><br>
 			    				<a href="www.surf-forecast.com/regions/Tortola-BVI" target="_blank">www.surf-forecast.com</a></p>
 			    			</div>
-			    			<div class="small-12 medium-8 large-9 cell">
-								<h1>FAQ</h1>
+			    			<div class="small-12 medium-8 large-8 cell">
+								<h1 class="title--page">FAQ</h1>
 								<!-- Flexible content -->
 								<?php while(has_sub_field('new_faq_content')): ?>
 									<?php if(get_row_layout() == 'qa_section'): ?>
@@ -31,7 +31,7 @@ get_header(); ?>
 											<div class="grid-x grid-padding-x">
 												<div class="small-12 medium-12 large-12 cell">
 													<section class="faq-qanda">
-														<h3><?php the_sub_field('h3_subheading'); ?><h3>
+														<h3 class="title--subheading"><?php the_sub_field('h3_subheading'); ?></h3>
 														<?php if( have_rows('faq_qanda') ): // Repeater 02 Field Name ?>
 															<?php while( have_rows('faq_qanda') ): the_row(); ?>
 																<h5>Q.&nbsp;<?php the_sub_field('faq_question'); ?></h5>
