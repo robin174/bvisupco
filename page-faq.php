76cc3d9ca829b9ffb2e29a6c915683beb6009ed8
Robin Green <robin@designingreen.co.uk>
2020-06-16 14:20:00 +0100

    missing https://

--- page-faq.php
+++ page-faq.php
@@ -19,7 +19,7 @@ get_header(); ?>
 			    				<p>For wind, wave and weather prediction models:<br>
 			    				<a href="https://www.windguru.cz/58" title="Wind Guru - Cane Garden Bay" target="_blank"><strong><i class="fas fa-external-link-alt"></i>&nbsp;&nbsp;www.windguru.cz</strong></a><br>
 			    				<a href="https://magicseaweed.com/British-Virgin-Islands-Surfing/133/" target="_blank"><strong><i class="fas fa-external-link-alt"></i>&nbsp;&nbsp;www.magicseaweed.com</strong></a><br>
-			    				<a href="www.surf-forecast.com/regions/Tortola-BVI" target="_blank"><strong><i class="fas fa-external-link-alt"></i>&nbsp;&nbsp;www.surf-forecast.com</strong></a></p>
+			    				<a href="https://www.surf-forecast.com/regions/Tortola-BVI" target="_blank"><strong><i class="fas fa-external-link-alt"></i>&nbsp;&nbsp;www.surf-forecast.com</strong></a></p>
 			    			</div>
 			    			<div class="small-12 medium-8 large-8 cell">
 								<h1 class="title--page">FAQs</h1>
