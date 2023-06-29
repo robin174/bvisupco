fce49338ec5af517e8cc37cef04ebb8ef4820691
Robin Green <robin@designingreen.co.uk>
2023-06-29 12:09:10 +0100

    Update GA code

--- header.php
+++ header.php
@@ -41,14 +41,14 @@
 		<!-- Roboto via Google Fonts -->
 		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-169415233-1"></script>
+		<!-- New for 2023: Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJ6NPN4BQ6"></script>
 		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
 
-		  gtag('config', 'UA-169415233-1');
+			gtag('config', 'G-CJ6NPN4BQ6');
 		</script>
 
 		<?php wp_head(); ?>
